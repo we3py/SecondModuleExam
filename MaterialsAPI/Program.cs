10a28093d7ca855a6c878213a21ab9cf856c6617
we3py <CRV374@motorolasolutions.com>
2022-08-08 14:11:59 +0200

    Add repositories and UoW to IOC container

--- MaterialsAPI/Program.cs
+++ MaterialsAPI/Program.cs
@@ -1,5 +1,3 @@
-using MaterialsAPI.Data.Context;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSqlServer<MaterialsContext>(builder.Configuration.GetConnectionString("MaterialsDB"));
+builder.Services.AddRepositories();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
 var app = builder.Build();
