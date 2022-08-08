8273b81ce60278ac45a8da42f8f013451988bd37
we3py <CRV374@motorolasolutions.com>
2022-08-08 19:35:55 +0200

    Create Custom middleware for exception handling

---
+++ MaterialsAPI/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿namespace MaterialsAPI.Middlewares
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (KeyNotFoundException e)
+            {
+                _logger.LogError(e, e.Message);
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                _logger.LogError(e, e.Message);
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(e.Message);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Error occured");
+            }
+        }
+    }
+}
