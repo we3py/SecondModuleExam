e2ec262b783fe9aa9bc545929d6ca208510ad4a9
we3py <CRV374@motorolasolutions.com>
2022-08-08 14:06:32 +0200

    Create UoW interface and class which implement it

---
+++ MaterialsAPI.Data/DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MaterialsAPI.Data.DAL
+{
+    public interface IUnitOfWork
+    {
+        IAuthorRepository AuthorRepository { get; set; }
+        IMaterialReviewRepository MaterialReviewRepository { get; set; }
+        IMaterialsRepository MaterialsRepository { get; set; }
+    }
+}
---
+++ MaterialsAPI.Data/DAL/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿namespace MaterialsAPI.Data.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IAuthorRepository AuthorRepository { get; set; }
+        public IMaterialReviewRepository MaterialReviewRepository { get; set; }
+        public IMaterialsRepository MaterialsRepository { get; set; }
+
+        public UnitOfWork(IAuthorRepository authorRepository, IMaterialReviewRepository materialReviewRepository, IMaterialsRepository materialsRepository)
+        {
+            AuthorRepository = authorRepository;
+            MaterialReviewRepository = materialReviewRepository;
+            MaterialsRepository = materialsRepository;
+        }
+
+    }
+}
