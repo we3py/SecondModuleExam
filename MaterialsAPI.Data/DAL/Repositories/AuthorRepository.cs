ebe7a3d9877f0bf7dfae1899df853170d8c717fc
we3py <CRV374@motorolasolutions.com>
2022-08-08 13:51:57 +0200

    Create repository for Authors

---
+++ MaterialsAPI.Data/DAL/Repositories/AuthorRepository.cs
@@ -0,0 +1,23 @@
+﻿using MaterialsAPI.Data.Context;
+using MaterialsAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialsAPI.Data.DAL.Repositories
+{
+    public class AuthorRepository : Repository<Author>
+    {
+        public AuthorRepository(MaterialsContext materialsContext) : base(materialsContext)
+        {
+        }
+
+        public async Task<ICollection<Author>> GetAll()
+            => await MaterialContext.Authors
+            .Include(a => a.Materials)
+            .ToListAsync();
+    }
+}
