62b2d743e5e661d921e5d74353710de9fd5e2bde
we3py <CRV374@motorolasolutions.com>
2022-08-08 14:48:11 +0200

    Create MaterialsController

---
+++ MaterialsAPI/Controllers/MaterialsController.cs
@@ -0,0 +1,49 @@
+﻿using MaterialsAPI.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialsController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Roles = "admin, user")]
+        public async Task<List<Material>> GetMaterials()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("[controller]/id")]
+        [Authorize(Roles = "admin, user")]
+        public async Task<Material> GetMaterial(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task AddMaterial(Material material)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "admin")]
+        public async Task UpdateMaterial(Material material)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Route("[controller]/id")]
+        [Authorize(Roles = "admin")]
+        public async Task DeleteMaterial(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
