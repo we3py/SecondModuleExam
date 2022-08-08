4b90ab56a553be81977f5134dd63b9d9015de4fb
we3py <CRV374@motorolasolutions.com>
2022-08-08 13:52:05 +0200

    Create repository for Reviews

---
+++ MaterialsAPI.Data/DAL/Repositories/MaterialReviewRepository.cs
@@ -0,0 +1,23 @@
+﻿using MaterialsAPI.Data.Context;
+using MaterialsAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialsAPI.Data.DAL.Repositories
+{
+    public class MaterialReviewRepository : Repository<MaterialReview>
+    {
+        public MaterialReviewRepository(MaterialsContext materialsContext) : base(materialsContext)
+        {
+        }
+
+        public async Task<ICollection<MaterialReview>> GetAll()
+            => await MaterialContext.MaterialReviews
+            .Include(mr => mr.Material)
+            .ToListAsync();
+    }
+}
