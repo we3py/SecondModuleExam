168635d79f88e805773ef551c2b372b5c3eca6ec
we3py <CRV374@motorolasolutions.com>
2022-08-08 12:40:40 +0200

    Create context class

---
+++ MaterialsAPI.Data/Context/MaterialsContext.cs
@@ -0,0 +1,35 @@
+﻿using MaterialsAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialsAPI.Data.Context
+{
+    public class MaterialsContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<MaterialReview> MaterialReviews { get; set; }
+        public DbSet<MaterialType> MaterialTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasKey(a => a.Id);
+            modelBuilder.Entity<Material>().HasKey(m => m.Id);
+            modelBuilder.Entity<MaterialReview>().HasKey(mr => mr.Id);
+            modelBuilder.Entity<MaterialType>().HasKey(mt => mt.Id);
+
+            modelBuilder.Entity<Material>()
+                .HasMany(m => m.Reviews)
+                .WithOne(r => r.Material);
+
+            modelBuilder.Entity<Material>()
+                .HasOne(m => m.Author)
+                .WithMany(a => a.Materials);
+        }
+
+    }
+}
