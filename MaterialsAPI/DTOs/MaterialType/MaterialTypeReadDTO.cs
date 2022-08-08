0d8375ddefae850efcf205b2d5fffdb9b2d4fa24
we3py <CRV374@motorolasolutions.com>
2022-08-08 15:39:21 +0200

    Create DTO for MaterialType read

---
+++ MaterialsAPI/DTOs/MaterialType/MaterialTypeReadDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MaterialsAPI.DTOs.MaterialType
+{
+    public class MaterialTypeReadDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Definition { get; set; }
+    }
+}
