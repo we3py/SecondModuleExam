f5214ac67f37ee156a301ee9116000535508ab0a
we3py <CRV374@motorolasolutions.com>
2022-08-08 12:41:28 +0200

    Add SQL server to services

--- MaterialsAPI/Program.cs
+++ MaterialsAPI/Program.cs
@@ -1,3 +1,5 @@
+using MaterialsAPI.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<MaterialsContext>(builder.Configuration.GetConnectionString("MaterialsDB"));
+
 
 var app = builder.Build();
 
