c687347f085c335d81f3d7f058c0592dfeb304ec
we3py <CRV374@motorolasolutions.com>
2022-08-08 15:37:31 +0200

    Create DTO for Author craete and update

---
+++ MaterialsAPI/DTOs/Author/AuthorCreateUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MaterialsAPI.DTOs.Author
+{
+    public class AuthorCreateUpdateDTO
+    {
+        public string AuthorName { get; set; }
+        public string Description { get; set; }
+    }
+}
