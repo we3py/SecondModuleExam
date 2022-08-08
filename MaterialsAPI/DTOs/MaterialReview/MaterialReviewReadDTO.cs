ae9188523b847e5bdcd8931912c82b46e2e4afa2
we3py <CRV374@motorolasolutions.com>
2022-08-08 15:42:11 +0200

    Create DTO for MaterialReview read

---
+++ MaterialsAPI/DTOs/MaterialReview/MaterialReviewReadDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MaterialsAPI.DTOs.MaterialReview
+{
+    public class MaterialReviewReadDTO
+    {
+        public int Id { get; set; }
+        public string MaterialName { get; set; }
+        public string Review { get; set; }
+        public int Rating { get; set; }
+    }
+}
