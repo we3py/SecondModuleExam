72a1a5c0646d072e965f6645ed1f15bcec3997c4
we3py <CRV374@motorolasolutions.com>
2022-08-08 15:47:48 +0200

    Create DTO for Material create and update

---
+++ MaterialsAPI/DTOs/Material/MaterialCreateUpdateDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MaterialsAPI.DTOs.Material
+{
+    public class MaterialCreateUpdateDTO
+    {
+        public int AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public int MaterialTypeId { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
