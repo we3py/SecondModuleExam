db4c116a34f570d2e390761237e0a8b87dbf8464
we3py <CRV374@motorolasolutions.com>
2022-08-08 15:42:24 +0200

    Create DTO for MaterialReview create and update

---
+++ MaterialsAPI/DTOs/MaterialReview/MaterialReviewCreateUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MaterialsAPI.DTOs.MaterialReview
+{
+    public class MaterialReviewCreateUpdateDTO
+    {
+        public int MaterialId { get; set; }
+        public string Review { get; set; }
+        public int Rating { get; set; }
+    }
+}
