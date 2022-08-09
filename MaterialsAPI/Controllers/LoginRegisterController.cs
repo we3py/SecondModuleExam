ee81912b23633b3ab41588c32fcf6c74c4231918
we3py <CRV374@motorolasolutions.com>
2022-08-09 10:01:00 +0200

    Create controller for registering and logging

---
+++ MaterialsAPI/Controllers/LoginRegisterController.cs
@@ -0,0 +1,42 @@
+﻿using MaterialsAPI.DTOs.User;
+using MaterialsAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginRegisterController : ControllerBase
+    {
+        private LoggingRegisterService _loggingRegisterService;
+
+        public LoginRegisterController(LoggingRegisterService loggingRegisterService)
+        {
+            _loggingRegisterService = loggingRegisterService;
+        }
+
+        [HttpGet]
+        [Route("LogIn")]
+        public async Task<IActionResult> LogIn(string name, string password)
+        {
+            var JWT = await _loggingRegisterService.LogIn(name, password);
+            return Ok(JWT);
+        }
+
+        [HttpPost]
+        [Route("registerAdmin")]
+        public async Task<IActionResult> RegisterAdmin(UserRegisterDTO admin)
+        {
+            var registeredUser = await _loggingRegisterService.RegisterAdmin(admin);
+            return Created("", $"User with {registeredUser} role was registered");
+        }
+
+        [HttpPost]
+        [Route("registerUser")]
+        public async Task<IActionResult> RegisterUser(UserRegisterDTO user)
+        {
+            var registeredUser = await _loggingRegisterService.RegisterUser(user);
+            return Created("", $"User with {registeredUser} role was registered");
+        }
+    }
+}
