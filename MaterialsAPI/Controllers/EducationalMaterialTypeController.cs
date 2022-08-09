deeb3d8c50549058b74463b7bc50466e2efc0007
we3py <CRV374@motorolasolutions.com>
2022-08-09 10:44:24 +0200

    Create controller for material type

---
+++ MaterialsAPI/Controllers/EducationalMaterialTypeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EducationalMaterialTypeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task gowno()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
