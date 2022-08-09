8fb84a0780fe592f7afe2b63d70e55d9624b89b5
we3py <CRV374@motorolasolutions.com>
2022-08-09 09:58:50 +0200

    Create UserContext class

---
+++ MaterialsAPI.UsersDATA/Context/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using MaterialsAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialsAPI.Data.Context
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+        }
+
+    }
+}
