dbc4ecc5563f2149db617065c8e610227a347e12
we3py <CRV374@motorolasolutions.com>
2022-08-09 12:40:40 +0200

    Create Author controller with endpoints

---
+++ MaterialsAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,46 @@
+﻿using MaterialsAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace MaterialsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private IAuthorService _authorService;
+
+        public AuthorsController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [SwaggerOperation(Summary = "Get all authors")]
+        [HttpGet]
+        [Authorize(Roles = "admin, user")]
+        public async Task<IActionResult> GetAuthors()
+        {
+            return Ok(await _authorService.GetAllAuthorsAsync());
+        }
+
+        [SwaggerOperation(Summary = "Get author by ID")]
+        [HttpGet]
+        [Route("{id}")]
+        [Authorize(Roles = "admin, user")]
+        public async Task<IActionResult> GetAuthor(int id)
+        {
+            return Ok(await _authorService.GetAuthorAsync(id));
+        }
+
+        [SwaggerOperation(Summary = "Get material from author with average rating more than 5")]
+        [HttpGet]
+        [Route("{id}/Materials")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> GetMaterialsWithAverageMoreThanFiveFromAuthor(int id)
+        {
+            return Ok(await _authorService.GetMaterialWithAverageMoreThanFiveByAuthorAsync(id));
+        }
+    }
+}
