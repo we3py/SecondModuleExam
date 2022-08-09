e4f17d2625254cd909ae7231f526708204585278
we3py <CRV374@motorolasolutions.com>
2022-08-09 10:34:27 +0200

    Create method for configuring swagger

--- MaterialsAPI/Utilties/ExtensionMethods.cs
+++ MaterialsAPI/Utilties/ExtensionMethods.cs
@@ -1,6 +1,7 @@
 ﻿using MaterialsAPI.Data.DAL.Interfaces;
 using MaterialsAPI.Services;
 using MaterialsAPI.UsersDATA;
+using Microsoft.OpenApi.Models;
 
 namespace MaterialsAPI.Utilties
 {
@@ -18,6 +19,46 @@ namespace MaterialsAPI.Utilties
         {
             services.AddScoped<EducationMaterialService>();
             services.AddScoped<LoggingRegisterService>();
+        }
+
+        public static void BuildSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("ex2", new OpenApiInfo
+                {
+                    Version = "ex2",
+                    Title = "SecondExamAPI",
+                    Description = "web api made for utilize education materials"
+                });
+
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+
+                swagger.EnableAnnotations();               
+            });
         }
     }
 }
